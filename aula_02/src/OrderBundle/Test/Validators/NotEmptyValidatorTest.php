9a7be96f01a03a728373f762aec453c888391bee
frinhanifrinhani <frinhaniconsultoria@gmail.com>
2020-07-03 20:38:24 -0300

    AssertTrue e AssertFalse NotEmptyValidator()

---
+++ aula_02/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OrderBundle\Validators\Test;
+
+use OrderBundle\Validators\NotEmptyValidator;
+use PHPUnit\Framework\TestCase;
+
+class NotEmptyValidatorTest extends TestCase
+{
+
+    public function testShouldNotBeValidWhenValueIsEmpty(){
+
+        $emptyValue = "";
+        $notEmptyValidator = new NotEmptyValidator($emptyValue);
+
+        $isValid = $notEmptyValidator->isValid();
+
+        $this->assertFalse($isValid);
+    }
+
+    public function testShouldBeValidWhenValueIsEmpty(){
+
+        $notEmptyValue = "food";
+        $notEmptyValidator = new NotEmptyValidator($notEmptyValue);
+
+        $isValid = $notEmptyValidator->isValid();
+
+        $this->assertTrue($isValid);
+    }
+
+}
