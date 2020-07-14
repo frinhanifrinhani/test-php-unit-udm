be2084daf364e8f56f9cb6698077cfeaa3d4030b
frinhanifrinhani <frinhaniconsultoria@gmail.com>
2020-07-14 20:42:53 -0300

    Expect Exception

--- aula_02/src/PaymentBundle/Test/Service/PaymentServiceTest.php
+++ aula_02/src/PaymentBundle/Test/Service/PaymentServiceTest.php
@@ -6,6 +6,7 @@ namespace PaymentBundle\Test\Service;
 use OrderBundle\Entity\CreditCard;
 use OrderBundle\Entity\Customer;
 use OrderBundle\Entity\Item;
+use PaymentBundle\Exception\PaymentErrorException;
 use PaymentBundle\Repository\PaymentTransactionRepository;
 use PaymentBundle\Service\Gateway;
 use PaymentBundle\Service\PaymentService;
@@ -42,5 +43,35 @@ class PaymentServiceTest extends TestCase
          $paymentService->pay($customer,$item,$creditCard);
 
      }
+
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionWhenGatewayFails()
+    {
+        $gateway = $this->createMock(Gateway::class);
+        $paymentTransactionRepository = $this->createMock(PaymentTransactionRepository::class);
+
+        $paymentService = new PaymentService($gateway,$paymentTransactionRepository);
+
+        $gateway
+            ->expects($this->atLeast(3))
+            ->method('pay')
+            ->will($this->onConsecutiveCalls(
+                false,false,false
+            ));
+
+        $paymentTransactionRepository
+            ->expects($this->never())
+            ->method('save');
+
+        $this->expectException(PaymentErrorException::class);
+        $customer = $this->createMock(Customer::class);
+        $item = $this->createMock(Item::class);
+        $creditCard = $this->createMock(CreditCard::class);
+
+        $paymentService->pay($customer,$item,$creditCard);
+
+    }
 
 }
